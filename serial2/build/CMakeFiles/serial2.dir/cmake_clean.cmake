4fd8ed29c6206b0d4fc37bd9b79feb3c6c47293a
skywoodsz <skywoodsz@stu.csust.edu.cn>
2019-03-28 21:20:31 +0800

    serial base on the cpp

---
+++ serial2/build/CMakeFiles/serial2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/serial2.dir/main.cpp.o"
+  "CMakeFiles/serial2.dir/pc2mcu.cpp.o"
+  "serial2.pdb"
+  "serial2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/serial2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
